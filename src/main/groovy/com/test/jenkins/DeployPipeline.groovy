68bcc178531545a20a0eec628ee2718ebb98beaf
manh.ha <manh.ha@gooddata.com>
2021-03-04 16:31:10 +0700

    Update pipeline script

--- src/main/groovy/com/test/jenkins/DeployPipeline.groovy
+++ src/main/groovy/com/test/jenkins/DeployPipeline.groovy
@@ -21,27 +21,22 @@ class DeployPipeline extends Pipeline {
         script.stage("Prepare") {
             script.echo("Hello, world")
             script.echo("The value of GlobalVars is : ${GlobalVars.name}")
-            script.parallel(
-                test: {
-                    test1: script.echo("1")
-                    test2: script.echo("2")
-                },
-                test2: {
-                    test3: script.echo("3")
-                    test4: script.echo("4")
-                }
-            )
-//            script.parallel(
-////                test2: script.node() {
-////                    script.sh("echo 2")
-////                }
-//            })
+            Map test = [:]
+            test['test1'] = {
+                test1: script.echo("1")
+                test2: script.echo("2")
+            }
+            test['test2'] = {
+                test3: script.echo("3")
+                test4: script.echo("4")
+            }
+            script.parallel(test)
         }
     }
     @NonCPS
     def nextPhase() {
         script.stage("Next") {
-            script.echo("The value of GlobalVars is : ${GlobalVars.name}")
+            script.echo("The value of GlobalVars is : ${GlobalVars.name} - End.")
 //            sayHello 'Job 1'
         }
     }
