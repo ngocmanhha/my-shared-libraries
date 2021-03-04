bf6c168fb688acb192957ccf44489414b3c3c972
manh.ha <manh.ha@gooddata.com>
2021-03-04 16:27:52 +0700

    Update pipeline script

--- src/main/groovy/com/test/jenkins/DeployPipeline.groovy
+++ src/main/groovy/com/test/jenkins/DeployPipeline.groovy
@@ -21,19 +21,16 @@ class DeployPipeline extends Pipeline {
         script.stage("Prepare") {
             script.echo("Hello, world")
             script.echo("The value of GlobalVars is : ${GlobalVars.name}")
-            script.parallel({
+            script.parallel(
                 test: {
                     test1: script.echo("1")
                     test2: script.echo("2")
-    //                test2: script.node() {
-    //                    script.sh("echo 2")
-    //                }
-                }
+                },
                 test2: {
                     test3: script.echo("3")
                     test4: script.echo("4")
                 }
-            })
+            )
 //            script.parallel(
 ////                test2: script.node() {
 ////                    script.sh("echo 2")
