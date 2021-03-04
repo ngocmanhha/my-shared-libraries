6afcf66a33c9aa053b4fbe3690c4e67c338719fd
manh.ha <manh.ha@gooddata.com>
2021-03-04 17:39:19 +0700

    Update pipeline script

--- src/main/groovy/com/test/jenkins/DeployPipeline.groovy
+++ src/main/groovy/com/test/jenkins/DeployPipeline.groovy
@@ -30,32 +30,16 @@ class DeployPipeline extends Pipeline {
 //                test3: script.echo("3")
 //                test4: script.echo("4")
 //            }
-            List<String> arr1 = new ArrayList<>();
-            arr1.add("1");
-            arr1.add("2");
-            List<String> arr2 = new ArrayList<>();
-            arr1.add("3");
-            arr1.add("4");
 //            test["test1"] = {
 //                arr1.forEach({ item -> script.echo("${item}") })
-//            }
-//            test["case1"] = {
-//                script.stage("test1") {
-//                    script.echo("1")
-//                    script.echo("2")
-//                }
-//            }
-//            test["case2"] = {
-//                script.stage("test2") {
-//                    script.echo("3")
-//                    script.echo("4")
-//                }
 //            }
             test["case1"] = {
                 script.stage("test1") {
                     script.echo("1")
                     script.echo("2")
                 }
+            }
+            test["case2"] = {
                 script.stage("test2") {
                     script.echo("3")
                     script.echo("4")
