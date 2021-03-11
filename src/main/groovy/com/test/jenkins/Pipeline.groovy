464a07dbb2d3f7b0f3cd6e107ea992661b14980f
manh.ha <manh.ha@gooddata.com>
2021-03-11 15:24:23 +0700

    Read config

--- src/main/groovy/com/test/jenkins/Pipeline.groovy
+++ src/main/groovy/com/test/jenkins/Pipeline.groovy
@@ -30,7 +30,8 @@ abstract class Pipeline implements Serializable {
         def configFile = ".jenkins-ci.yaml"
         Map configs = script.readYaml(text: script.readFile(file: configFile))
         script.echo("Loaded config yaml: ${configs}")
-        this.config.constants.pipeline = configs;
+        config.constants.pipeline = configs
+        script.echo(config)
         construct(script, config)
     }
 
