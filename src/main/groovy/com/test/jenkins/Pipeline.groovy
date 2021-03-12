634dfc75fa08b50751b042799805c062018a7c9e
manh.ha <manh.ha@gooddata.com>
2021-03-12 18:28:18 +0700

    Read config

--- src/main/groovy/com/test/jenkins/Pipeline.groovy
+++ src/main/groovy/com/test/jenkins/Pipeline.groovy
@@ -1,5 +1,7 @@
 package com.test.jenkins
 
+import javax.security.auth.login.Configuration
+
 abstract class Pipeline implements Serializable {
     Script script
     Map config = [
@@ -41,36 +43,12 @@ abstract class Pipeline implements Serializable {
 
         configuration.pipelineDefinition = config
         configuration.build = prepareBuildVariables(script, configuration)
-        return new DeployPipeline(script, configuration)
-    }
-
-    private static Map prepareBuildVariables(Script script, Map configuration) {
-        script.node('master') {
-            script.stage('prepare-build-variables') {
-                Map scmVars = script.checkout(script.scm)
-                def configFile = ".jenkins-ci.yaml"
-                Map configs = script.readYaml(text: script.readFile(file: configFile))
-                script.echo("Loaded config yaml: ${configs}")
-//                script.echo(config)
-//                return new
-            }
-        }
-    }
-
-    private static Pipeline construct(Script script) {
-        // resolve pipeline type
-        script.echo("Pipeline type")
-        return new DeployPipeline(script)
-    }
-
-    protected Pipeline startPipeline() {
-        cls = new Class<>(script);
-        def timeout = script.constants.pipeline.build.timeout
-        script.echo("${timeout.toString()}")
-
+        Map configs = [
+            constants: prepareBuildVariables(script, configuration)
+        ]
+        Map timeout = configs.constants.pipeline.build.timeout
         if (!timeout?.time) {
-//            pipeline.run()
-            return (Pipeline)cls
+            return new DeployPipeline(script, config)
         }
         if (timeout?.unit) {
             if (validateUnit(timeout.unit)) {
@@ -78,8 +56,7 @@ abstract class Pipeline implements Serializable {
                         time: timeout.time,
                         unit: timeout.unit
                 ) {
-//                    pipeline.run()
-                    return (Pipeline)cls
+                    return new DeployPipeline(script, config)
                 }
             }
             else {
@@ -91,6 +68,49 @@ abstract class Pipeline implements Serializable {
             throw new Exception("Missing unit config")
             return
         }
+//        return new DeployPipeline(script, configs)
+    }
+
+    private static Map prepareBuildVariables(Script script, Map configuration) {
+        script.node('master') {
+            script.stage('prepare-build-variables') {
+                Map scmVars = script.checkout(script.scm)
+                def configFile = ".jenkins-ci.yaml"
+                Map configs = script.readYaml(text: script.readFile(file: configFile))
+                script.echo("Loaded config yaml: ${configs}")
+                return configs
+            }
+        }
+    }
+
+    private static Pipeline construct(Script script) {
+        // resolve pipeline type
+        script.echo("Pipeline type")
+        return new DeployPipeline(script)
+    }
+
+    protected Pipeline startPipeline(Script script, Map config, Map timeout, Pipeline pipeline) {
+//        if (!timeout?.time) {
+//            return new DeployPipeline(script, config)
+//        }
+//        if (timeout?.unit) {
+//            if (validateUnit(timeout.unit)) {
+//                script.timeout(
+//                        time: timeout.time,
+//                        unit: timeout.unit
+//                ) {
+//                    return new DeployPipeline(script, config)
+//                }
+//            }
+//            else {
+//                throw new Exception("Timeout unit is not match any in ${TimeoutUnit.values().toString()}")
+//                return
+//            }
+//        }
+//        else {
+//            throw new Exception("Missing unit config")
+//            return
+//        }
     }
 
     protected def validateUnit(String timeoutUnit) {
